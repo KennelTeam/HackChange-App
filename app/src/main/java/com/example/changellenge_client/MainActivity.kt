096dca05e5d7e0e8015dbd22528e4fffc181cf67
AndrusovN <andrusov.n@gmail.com>
2021-11-27 18:47:03 +0300

    networker. inseption

--- app/src/main/java/com/example/changellenge_client/MainActivity.kt
+++ app/src/main/java/com/example/changellenge_client/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.changellenge_client
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Networker.setup(this)
+        Networker.sendRequest("authent", {text: String? -> Log.i("Test!!!", text.toString())})
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
---
+++ app/src/main/java/com/example/changellenge_client/Networker.kt
@@ -0,0 +1,40 @@
+package com.example.changellenge_client
+
+import android.content.Context
+import android.util.Log
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class Networker {
+    companion object {
+        private lateinit var requestQueue: RequestQueue
+
+        fun setup(context: Context) {
+            requestQueue = Volley.newRequestQueue(context)
+        }
+
+        fun sendRequest(url: String, callback: (a: String?) -> Unit) {
+            try {
+                //Log.i("Test!!!", context!!.toString())
+                //context!!.
+                val resultUrl = "http://18.133.117.201:5000" + "/" + url
+                val stringRequest = StringRequest(Request.Method.GET, resultUrl,
+                    { response -> callback(response) }, { callback(null) })
+                //Log.i("Test!!!", "data: " + data)
+
+                requestQueue.add(stringRequest)
+            } catch (e: android.os.NetworkOnMainThreadException) {
+                Log.i("Test!!!", "error: " + e.stackTraceToString())
+            }
+
+
+        }
+    }
+}
